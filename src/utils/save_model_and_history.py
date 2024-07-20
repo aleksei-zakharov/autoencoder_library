da351416476a1defa9288a3b95f62504c5fbae2a
aleksei-zakharov <aleksei.al.zakharov@gmail.com>
2024-07-20 17:16:14 +0200

    add save_model_and_history.py file

---
+++ src/utils/save_model_and_history.py
@@ -0,0 +1,21 @@
+import os
+import pickle
+
+
+def save_model_and_history(model,
+                           history,
+                           name):
+    
+    folder_path = os.path.join("../../models/mnist", name)
+    os.makedirs(folder_path, exist_ok=True)
+    
+    # Save the model
+    model_file_path = os.path.join(folder_path, 'model.keras') # .keras is recommended keras 3 format (see https://www.tensorflow.org/guide/keras/serialization_and_saving#registering_the_custom_object)
+    model.save(model_file_path)
+
+    # Save history to the history.pkl file
+    hist_file_path = os.path.join(folder_path, 'history.pkl')
+    with open(hist_file_path, 'wb') as file_pi:
+        pickle.dump(history, file_pi)
+
+
