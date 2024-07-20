8db9c9edbb47796e0c68ff5550cbbfa684458f81
aleksei-zakharov <aleksei.al.zakharov@gmail.com>
2024-07-20 15:11:12 +0200

    add save feature to plot_latent_space_2d_values.py

--- src/visualization/plot_latent_space_2d_values.py
+++ src/visualization/plot_latent_space_2d_values.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib.pyplot as plt
 from matplotlib import cm  # to change color scheme
 
@@ -5,7 +6,8 @@ from matplotlib import cm  # to change color scheme
 def plot_latent_space_2d_values(model, 
                                 x,
                                 y=None,
-                                vae_latent_type=None):
+                                vae_latent_type=None,
+                                save_name=None):
     # Because our latent space is two-dimensional, there are a few cool visualizations that can be done at this point. One is to look at the neighborhoods of different classes on the latent 2D plane
     if vae_latent_type is None:
         vae_latent_type = 'z'
@@ -29,16 +31,18 @@ def plot_latent_space_2d_values(model,
     else:
         plt.scatter(x_encoded[:,0], x_encoded[:,1], c=y, s=1, cmap=cm.rainbow)  # s=2 initially
     plt.colorbar()
-    # # save plot
-    # if save_name is not None:
-    #     # make a folder
-    #     folder_path = os.path.join('../reports', save_name)
-    #     os.makedirs(folder_path, exist_ok=True)  # make a folder if doesn't exist
-    #     # save plot if the file doesn't exist
-    #     file_path = os.path.join('../reports', save_name, 'predictions_of_latent_space.png')
-    #     if os.path.exists(file_path):
-    #         raise FileExistsError(f"The file '{file_path}' already exists.")
-    #     else:
-    #         plt.savefig(file_path)
-    # display plot
+
+    # Save plot
+    if save_name is not None:
+        # Make a folder
+        folder_path = '../../reports/mnist'
+        os.makedirs(folder_path, exist_ok=True)  # make a folder if doesn't exist
+        # Save plot if the file doesn't exist
+        file_path = os.path.join(folder_path, save_name + '_lat_sp_2d.png')
+        if os.path.exists(file_path):
+            raise FileExistsError(f"The file '{file_path}' already exists.")
+        else:
+            plt.savefig(file_path)
+
+    # Display plot
     plt.show()
