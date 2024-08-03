1a4bcf6f3a847c3c10df7c3c54a73dbf529ae41f
aleksei-zakharov <aleksei.al.zakharov@gmail.com>
2024-08-03 12:50:00 +0200

    add cropping and fps=5 for gif_surfaces_by_dates

--- src/visualization/vol/gif_surfaces_by_dates.py
+++ src/visualization/vol/gif_surfaces_by_dates.py
@@ -4,6 +4,7 @@ import imageio
 import glob
 import os
 from IPython.display import Image, display
+from PIL import Image as ImagePIL
 
 
 def gif_surfaces_by_dates(data,
@@ -40,9 +41,16 @@ def gif_surfaces_by_dates(data,
         ax.set_zlabel('vol in bp')
         ax.set_zlim(min_vol, max_vol)
         date_string = f"{date.year}-{date.month:02}-{date.day:02}"
-        ax.set_title(f'atm vol surface on ' + date_string)
-        plt.savefig(folder_path + 'image_' + f"{i:04}" +'.png')  # save a plot as png image
+        ax.set_title(f'atm vol surface on ' + date_string + ' (in bp)')
+        file_path = folder_path + 'image_' + f"{i:04}" +'.png'
+        plt.savefig(file_path)  # save a plot as png image
         plt.close(fig)  # just not to show the graphs in python
+        # Crop the image
+        img = ImagePIL.open(file_path)
+        width, height = img.size
+        box = (int(0.15 * width), int(0.05 * height), int(0.9 * width), int(0.95 * height))  # left, top, right, bottom
+        img = img.crop(box)
+        img.save(file_path)
 
     # Create an array of saved plots
     images = []
@@ -50,7 +58,7 @@ def gif_surfaces_by_dates(data,
         images.append(imageio.imread(filename))
     # Save a gif
     gif_file_path = folder_path + 'plot_surfaces_by_dates.gif'
-    imageio.mimsave(gif_file_path, images, loop=0, fps=100)  # loop=0 means that gif will be played non-stop ; fps=100 is maximum
+    imageio.mimsave(gif_file_path, images, loop=0, fps=5)  # loop=0 means that gif will be played non-stop ; fps=100 is maximum
 
     # # Delete all created png files
     for filename in sorted(glob.glob(folder_path + '*.png')):
