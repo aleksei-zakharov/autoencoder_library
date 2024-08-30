ab9ddb9e83d897215a6f135ab07b77d3337efa83
aleksei-zakharov <aleksei.al.zakharov@gmail.com>
2024-08-30 22:44:33 +0200

    extend plot_true_vs_prediction_images to draw graph for pca model too

--- src/visualization/mnist/plot_true_vs_prediction_images.py
+++ src/visualization/mnist/plot_true_vs_prediction_images.py
@@ -1,5 +1,6 @@
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_true_vs_prediction_images(model, 
@@ -9,7 +10,7 @@ def plot_true_vs_prediction_images(model,
     # Graph shows comparison between original pictures and predicted pictures
     N = 5  # number of samples to be shown for each value 0, 1, 2, ... 9
 
-    plt.figure(figsize=(13, 5))
+    plt.figure(figsize=(13, 3.2))
     for n_col in range(10):  # digits 0, 1, 2, ..., 9
         idx = 0 # index of current element in X_train
         for n_row in range(N):  # samples
@@ -24,7 +25,14 @@ def plot_true_vs_prediction_images(model,
             ax.get_yaxis().set_visible(False)
 
             # Display picture deconstred via autoencoder
-            decoded_img = model.predict(x[idx][None,...], verbose=0) # to predict only 1 element. The result has (1,784) shape 
+            if model.model_type in ['vae','ae']:
+                decoded_img = model.predict(x[idx][None,...], verbose=0) # to predict only 1 element. The result has (1,784) shape 
+            elif model.model_type == 'pca':
+                # project the dataset onto the principal components
+                pca_comp = model.transform(np.expand_dims(x[idx],0))
+                # transform the projections back into the input space
+                pca_recon = model.inverse_transform(pca_comp)
+                decoded_img = pca_recon  # to predict only 1 element. The result has (1,784) shape 
             ax = plt.subplot(N, 20, n_row*20 + n_col*2 + 2)  # number of rows, number of columns, graph index
             plt.imshow(decoded_img.reshape(28, 28))
             plt.gray()
@@ -34,11 +42,11 @@ def plot_true_vs_prediction_images(model,
 
     # Save plot
     if save_name is not None:
-        # Make a folder
+        # Make a folder if it doesn't exist
         folder_path = '../../reports/mnist'
-        os.makedirs(folder_path, exist_ok=True)  # make a folder if doesn't exist
-        # Save plot if the file doesn't exist
-        file_path = os.path.join(folder_path, save_name + '_prediction_images.png')
+        os.makedirs(folder_path, exist_ok=True)
+        # Save plot
+        file_path = os.path.join(folder_path, save_name + '_true_vs_prediction_images.png')
         plt.savefig(file_path)
             
     # Display plot
