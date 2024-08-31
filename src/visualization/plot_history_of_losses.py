62d3efdcdc4a0f42576d6ff0ad2e5afd88545261
aleksei-zakharov <aleksei.al.zakharov@gmail.com>
2024-08-31 13:40:43 +0200

    add comments to functions

--- src/visualization/plot_history_of_losses.py
+++ src/visualization/plot_history_of_losses.py
@@ -4,10 +4,22 @@ import numpy as np
 
 
 def plot_history_of_losses(history,
-                           data_type='mnist',  # type = 'mnist' or 'vol'
+                           data_type,
                            save_name=None):
+    """
+    Plot history of log of losses for train and test dataset
+
     
-    # graph shows logarithm of total losses for train and test datasets
+    Parameters:
+
+    history: history from keras.model fitting
+    
+    data_type: type of data to be plotted. Possible values: 'mnist' or 'vol'. The plot is saved in the folder with this name
+
+    save_name: the name of the trained model that is used here to name the saved plot. If it is not None, the plot is saved in the folder 
+    """
+
+    # Graph shows logarithm of total losses for train and test datasets
     plt.plot(np.log(history.history['total_loss']))
     if 'val_total_loss' in history.history.keys():
         plt.plot(np.log(history.history['val_total_loss']))
@@ -20,10 +32,10 @@ def plot_history_of_losses(history,
 
     # Save plot
     if save_name is not None:
-        # Make a folder
+        # Make a folder if it doesn't exist
         folder_path = '../../reports/' + data_type
-        os.makedirs(folder_path, exist_ok=True)  # make a folder if doesn't exist
-        # Save plot if the file doesn't exist
+        os.makedirs(folder_path, exist_ok=True) 
+        # Save plot
         file_path = os.path.join(folder_path, save_name + 'history.png')
         plt.savefig(file_path)
     
--- src/visualization/plot_latent_space_1d_values.py
+++ src/visualization/plot_latent_space_1d_values.py
@@ -6,14 +6,33 @@ import os
 
 def plot_latent_space_1d_values(model, 
                                 x, 
+                                data_type,
                                 y=None,
                                 vae_latent_type=None,
-                                data_type='mnist',  # type = 'mnist' or 'vol'
                                 save_name=None):
+    """
+    Draw the histogram of latent space variables calculated from inputs
+
+    
+    Parameters:
+
+    model: the model such as autoencoder (keras.Model), variational autoencoder (keras.Model) or PCA method
+    
+    x: the inputs of the model (mnist dataset of handwritten-digit images or volatility cube data)
+    
+    data_type: type of data to be plotted. Possible values: 'mnist' or 'vol'. The plot is saved in the folder with this name
+
+    y: the labels of input data. It is used only for mnist dataset: the real digits such as 0, 1, ..., 9.
+
+    vae_latent_type: type of latent space variable that must be plotted. Possible values: 'z', 'z_mean', 'z_logvar'.
+
+    save_name: the name of the trained model that is used here to name the saved plot. If it is not None, the plot is saved in the folder 
+    """
 
     if vae_latent_type is None:
         vae_latent_type = 'z'
 
+    # The calculation of latent space variables for different model types
     if model.model_type == 'vae':
         if vae_latent_type == 'z_mean':
             x_encoded, __, __ = model.encoder.predict(x, verbose=0)  # z_mean, z_logvar, z
@@ -24,8 +43,8 @@ def plot_latent_space_1d_values(model,
     elif model.model_type == 'ae':
         x_encoded = model.encoder.predict(x, verbose=0)  # z
 
+    # Histogram of the latent space variable
     bins = np.linspace(np.min(x_encoded), np.max(x_encoded), 400)
-    
     if y is None:
         plt.hist(x_encoded, bins, alpha=0.5)
     else:
