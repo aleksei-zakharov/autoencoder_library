3eb0ebc89614db4e8ab8b298b07e3270546c3b81
aleksei-zakharov <aleksei.al.zakharov@gmail.com>
2024-10-19 20:27:38 +0200

    add logarithmization and comments to normalizer.py

--- src/data/vol/normalizer.py
+++ src/data/vol/normalizer.py
@@ -1,7 +1,24 @@
-import numpy as np
+from numpy import log, exp
 
 
 class Normalizer():
+    """
+    2 aims of Normalizer class:
+
+        1) To apply logarithm of data to make data more dispersed and not concentrated 
+        in low values, which improves data distinguishability.
+
+        2) To apply min-max scaler to increase the speed of convergence of neural network
+    
+    Methods:
+
+        normalize: returns normalized data: take logarithm of data and then apply 
+        min-max normalization to get data in [0,1] interval
+
+        denormalize: returns initial values from normalized data: apply the inverse 
+        of min-max normalization and take exponent    
+    """
+
     def __init__(self):
         self.min = None
         self.max = None
@@ -11,7 +28,7 @@ class Normalizer():
         data_ = data
         
         # Logarithm
-        # data_ = np.log(data_)
+        data_ = log(data_)
 
         # Minmax scaler
         self.min = data_.min()
@@ -30,6 +47,6 @@ class Normalizer():
         data_ = data_ + self.min
         
         # The action opposite to the logarithm in normalize function above
-        # data_ = np.exp(data_)
+        data_ = exp(data_)
         
         return data_
