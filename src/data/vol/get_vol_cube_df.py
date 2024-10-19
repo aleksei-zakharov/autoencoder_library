2ad04a0deb1ba76af5ef673861e466784d2840d3
aleksei-zakharov <aleksei.al.zakharov@gmail.com>
2024-10-19 20:31:44 +0200

    add comment to get_vol_cube_df.py

--- src/data/vol/get_vol_cube_df.py
+++ src/data/vol/get_vol_cube_df.py
@@ -1,11 +1,16 @@
-import pandas as pd
+from pandas import read_excel
 
 from references.global_parameters import SAVE_VOL_CUBE_FILE_PATH, \
                                          SAVE_VOL_CUBE_FILE_SHEET_NAME
 
 
-def get_vol_cube_df():    
-    df = pd.read_excel(SAVE_VOL_CUBE_FILE_PATH, 
+def get_vol_cube_df():
+    """
+    Download (from excel file ) DataFrame with volatilities for different strikes, 
+    opt and swap tenors and dates 
+    """
+    
+    df = read_excel(SAVE_VOL_CUBE_FILE_PATH, 
                        sheet_name=SAVE_VOL_CUBE_FILE_SHEET_NAME,
                        index_col=0)
     return df
