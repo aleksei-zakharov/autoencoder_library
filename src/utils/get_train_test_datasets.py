b64e309b22df933e7dc4861406c6696181c4fc0e
aleksei-zakharov <aleksei.al.zakharov@gmail.com>
2024-10-19 20:28:26 +0200

    change get_train_test_datasets.py to split randomly and by time

--- src/utils/get_train_test_datasets.py
+++ src/utils/get_train_test_datasets.py
@@ -1,26 +1,31 @@
 import numpy as np
-
+import datetime
 
 def get_train_test_datasets(data,   # np.array
                             dates,  # list
-                            seed,
-                            train_ratio=0.8):
+                            type='random_split',  # 'random' or 'test_after_2021' or 'test_after_covid'
+                            seed=0,
+                            train_ratio=0.8):  
     
-    # Generate a random permutation of indices
-    num_samples = data.shape[0]
-    np.random.seed(seed)
-    indices = np.random.permutation(num_samples)
+    if type == 'random_split':
+        # Generate a random permutation of indices
+        num_samples = data.shape[0]
+        np.random.seed(seed)
+        indices = np.random.permutation(num_samples)
 
-    # Determine the split index
-    split_index = int(train_ratio * num_samples)
+        # Determine the split index
+        split_index = int(train_ratio * num_samples)
 
-    # Split the indices for training and testing sets
-    train_indices = indices[:split_index]
-    test_indices = indices[split_index:]
+        # Split the indices for training and testing sets
+        train_indices = indices[:split_index]
+        test_indices = indices[split_index:]
 
-    # Sort the indices for training and testing sets
-    train_indices.sort()
-    test_indices.sort()
+        # Sort the indices for training and testing sets
+        train_indices.sort()
+        test_indices.sort()
+    elif type == 'temporal_split':
+        train_indices = [i for i, val in enumerate(dates) if val.year < 2021]
+        test_indices =  [i for i, val in enumerate(dates) if val.year > 2021]      
 
     # Use the indices to create the train dataset
     data_train = data[train_indices]
